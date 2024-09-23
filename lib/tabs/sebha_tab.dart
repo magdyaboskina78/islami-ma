317444ff575ef177bc520eaa0b1a70d7bc3a6e4b
Mostafa Mahmoud <mostafax20202@gmail.com>
2024-09-23 14:15:17 +0300

    Creation of Quran tab and Ahadeth tab Creation sura_details_screen and ahadth_screen

---
+++ lib/tabs/sebha_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SebhaTab extends StatelessWidget {
+  const SebhaTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.blue,
+    );
+  }
+}
